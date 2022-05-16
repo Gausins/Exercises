dc5c70bb77c45ad3c6293cb9f7682165c13c23e9
Gausins <andris.gausins@gmail.com>
2022-05-16 15:34:55 +0300

    No23

--- Exercises/Level1/Exercises.Level1/Array1.cs
+++ Exercises/Level1/Exercises.Level1/Array1.cs
@@ -218,7 +218,11 @@ public class Array1
     /// </summary>
     public bool No23(int[] nums)
     {
-        throw new NotImplementedException();
+        if (nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3)
+        {
+            return false;
+        }
+        return true;
     }
 
     /// <summary>
